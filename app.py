61f73238564f57a67fc2e5a2d260496ae162ff8a
Lutfi Farid <lutfi.faridd@gmail.com>
2026-08-16 21:17:30 +0700

    Change GEMINI_API_KEY value
    Updated GEMINI_API_KEY to a new value.

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'lutfifarid'
 ADMIN_LOGIN_KEY = 'lutfifarid'
 
 # API Key bawaan server untuk AI Tools (Enhancer, Img2Prompt, VIP Duo, VIP Solo)
-GEMINI_API_KEY = "AQ.Ab8RN6KttJ1MkuLtGUt5McQcPrbj4LytE1zg6ruDkHlKFURtow"
+GEMINI_API_KEY = "AQ.Ab8RN6LMG8hz27fCD0DeGoU5ZFpeZoDDzGPniHSMjE2zvXkwMA"
 
 # String Koneksi MongoDB Atlas
 # Silakan ganti URL di bawah ini dengan URI MongoDB Atlas kamu:
